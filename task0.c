310b25ad9938dc1452a863f4cbe545a746be0499
Hafsah-Awal <awal.hafsat2000@gmail.com>
2023-04-16 04:46:10 -0700

    task0

---
+++ task0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to check
+*  Owned by Hafsah/Victor
+*  Return: 0 is success
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
