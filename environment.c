a5c81076d037d614d51786b81c86b1c61c3d6285
IamTalah <viktormbewe@gmail.com>
2023-04-23 10:42:53 -0700

    prints the environment

---
+++ environment.c
@@ -0,0 +1,19 @@
+#include "head.h"
+
+/**
+ * print_environ - prints the environment variables to stdout
+ * Return: void
+*/
+void print_environ(void)
+{
+	int x = 0;
+	char **env = environ;
+
+	while (env[x])
+	{
+		write(STDOUT_FILENO, (const void *)env[x], _strlen(env[x]));
+		write(STDOUT_FILENO, "\n", 1);
+		x++;
+	}
+}
+
