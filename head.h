02a02e4cb7a3f8b16b43c8cd6e25712be2517024
IamTalah <viktormbewe@gmail.com>
2023-04-23 11:46:36 -0700

    header file

---
+++ head.h
@@ -0,0 +1,65 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdbool.h>
+
+/* environment variables */
+extern char **environ;
+extern __sighandler_t signal(int __sig, __sighandler_t __handler);
+
+/* handle built ins */
+int check_builtin(char **cmd, char *buf);
+void print_shell_prompt(void);
+void signal_handler(int sig_num);
+char **tokenize_input(char *ln);
+char *check_valid_path(char **path, char *cmd);
+char *get_full_path(char *path, char *cmd);
+int execute_builtin_command(char **cmd, char *ln);
+void handle_exit_command(char **cmd, char *ln);
+
+void print_environ(void);
+
+/* string handlers */
+int string_compare(char *s1, char *s2);
+int string_length(char *s);
+int string_compare_2(char *s1, char *s2, int n);
+char *duplicate_string(char *s);
+char *find_char(char *s, char c);
+
+void execute_command(char *cp, char **cmd);
+char *get_path(void);
+int _putchar(char c);
+int print_string(char *s);
+int main(int ac, char **av, char *envp[]);
+
+/* helper function for efficient free */
+void buffer_cleaner(char **buf);
+
+struct builtin
+{
+	char *env;
+	char *exit;
+} builtin;
+
+struct info
+{
+	int final_exit;
+	int ln_count;
+} info;
+
+struct flags
+{
+	bool interactive;
+} flags;
+
+#endif /* SHELL_H */
+
