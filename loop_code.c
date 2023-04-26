f48b0dea508f04e735fb0bcb779e5d7daa9d8e4b
IamTalah <viktormbewe@gmail.com>
2023-04-26 05:54:30 -0700

    updated loop code

--- loop_code.c
+++ loop_code.c
@@ -106,7 +106,8 @@ void find_command(info_t *info)
 	}
 	else
 	{
-		if ((interactive(info) || _getenv(info, "PATH=") || info->argv[0][0] == '/') && is_exe_cmd(info, info->argv[0]))
+		if ((interactive(info) || _getenv(info, "PATH=")
+		|| info->argv[0][0] == '/') && is_exe_cmd(info, info->argv[0]))
 			fork_command(info);
 		else if (*(info->arg) != '\n')
 		{
