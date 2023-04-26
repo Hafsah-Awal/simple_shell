215ef631b607892f5774d6732d4486a262782b6d
IamTalah <viktormbewe@gmail.com>
2023-04-26 05:29:40 -0700

    kj

--- task0.c
+++ task0.c
@@ -1,16 +1,1 @@
 #include "head.h"
-
-/**
-*  print_message - prints a message
-*  @s: string to print
-*  Owned by Hafsah/Victor
-*  Return: 0 is success
-*/
-
-void print_message(char s)
-{
-	int message_len = strlen(&s);
-
-	write(STDOUT_FILENO, &s, message_len);
-}
-
